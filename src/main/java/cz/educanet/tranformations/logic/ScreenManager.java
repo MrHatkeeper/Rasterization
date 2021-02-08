9c0c16e5044b847a03b5823f18b7cc3a9b915066
MrHatkeeper <58586406+MrHatkeeper@users.noreply.github.com>
2021-02-08 16:30:24 +0100

    idk wtf am I doin'

--- src/main/java/cz/educanet/tranformations/logic/ScreenManager.java
+++ src/main/java/cz/educanet/tranformations/logic/ScreenManager.java
@@ -21,41 +21,37 @@ public class ScreenManager {
         return selectedPoints.contains(coordinate);
     }
 
-    public Set<Coordinate> getSelectedPoints() {
-        return selectedPoints;
-    }
-
-
-    private double rovnice(Coordinate fc, Coordinate sc) {
-        double a = ((double) sc.getY() - fc.getY()) / (sc.getX() - fc.getX());
-        double result = (double) sc.getY() - a * fc.getX();
+    private double rovnice(Coordinate x, Coordinate y) {
+        double a = ((double) y.getY() - x.getY()) / (y.getX() - x.getX());
+        double result = (double) x.getY() - (a * x.getX());
         return result;
     }
 
 
+    public Set<Coordinate> getSelectedPoints() {
+        return selectedPoints;
+    }
+
     public boolean isFilledIn(Coordinate coordinate) { // TODO: Implement this
         Coordinate[] body = getSelectedPoints().toArray(new Coordinate[getSelectedPoints().size()]);
 
-        // y = ax+b
-
-
         try {
+
 
             double a = rovnice(body[0], body[1]);
 
-            //double b = rovnice(body[1],body[2]);
-
-            //double c = rovnice(body[0],body[2]);
-
             double test = rovnice(body[0], coordinate);
 
-            if (a < test)
+            if(a < test){
                 return true;
+            }
 
 
         } catch (Exception e) {
-
         }
+        ;
+
         return false;
+
     }
 }
